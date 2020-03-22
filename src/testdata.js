2e40010bf8bf74bf637fd921bc765770494878ed
Toni Illokoski <toni.illokoski@gmail.com>
2020-03-22 23:09:09 +0200

    Lisätty testidata

---
+++ src/testdata.js
@@ -0,0 +1,37 @@
+const testdata = [
+    
+{ id: "1",
+tyyppi: 'Sähkö',
+summa: 437.50,
+maksupaiva: '2020-03-20',
+kaudenalku: '2020-12-01',
+kaudenloppu: '2020-02-28',
+saaja: 'Fortum' },
+
+{ id: "2",
+tyyppi: 'Vesi',
+summa: 352.00,
+maksupaiva: '2020-01-15',
+kaudenalku: '2020-10-01',
+kaudenloppu: '2020-12-31',
+saaja: 'Tampereen Vesilaitos' },
+
+{ id: "3",
+tyyppi: 'Puhelin',
+summa: 18.90,
+maksupaiva: '2020-01-12',
+kaudenalku: undefined,
+kaudenloppu: undefined,
+saaja: 'Telia' },
+
+{ id: "4",
+tyyppi: 'Vero',
+summa: 128.90,
+maksupaiva: '2020-02-01',
+kaudenalku: '2019-01-01',
+kaudenloppu: '2019-12-31',
+saaja: 'Verovirasto' }
+
+];
+
+export default testdata;
