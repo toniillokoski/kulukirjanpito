4908b96be0eccef9dde1a0a14630e4057d602290
Toni Illokoski <toni.illokoski@gmail.com>
2020-03-22 18:22:43 +0200

    projekti aloitettu, luonnosteltu tekstejä

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Kulukortti />
+        <Kulukortti />
+        <Kulukortti />
+    </div>
+  );
+}
+
+function Kulukortti(props) {
+  return (
+    <div className="kulukortti">
+      <div className="kulukortti__rivi">
+      <div className="kulukortti__tyyppi">Sähkö</div>
+      <div className="kulukortti__summa">437,50€</div>
+      </div>
+      <div className="kulukortti__rivi">
+      <div className="kulukortti__maksupaiva">20.3.2020</div>
+      <div className="kulukortti__ajanjakso">1.12.2019-28.2.2020</div>
+      </div>
+      <div className="kulukortti__rivi">
+      <div className="kulukortti__laskuttaja">Fortum</div>
+      <div className="kulukortti__keskiarvo">145,83€ / kk</div>
+      </div>
     </div>
   );
 }
