3c18b82a0b67e9b9ce54587a642054ada748e39e
Toni Illokoski <toni.illokoski@gmail.com>
2020-04-21 18:56:00 +0300

    Poistettu testdata käytöstä

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import testdata from './testdata';
+/* import testdata from './testdata'; */
 import firebase, { provider, auth } from './Firebase';
 
 import Header from './components/Header/Header';
